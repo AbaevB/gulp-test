fe359f267f74e0a36615a59933c7e617ef0857c2
AbaevB <abaev70@hotmail.com>
2024-08-31 05:32:59 +0700

    add tasks

--- project/gulpfile.js
+++ project/gulpfile.js
@@ -1,2 +1,21 @@
 const {src, dest, series, watch} = require('gulp');
 const concat = require('gulp-concat');
+const htmlMin = require('gulp-htmlmin');
+
+const styles = () => {
+    return src('src/css/**/*.css')
+        .pipe(concat('style.css'))
+        .pipe(dest('dist/ccss'))
+}
+
+const htmlMinify =  () => {
+   return src('src/**/*.html')
+        .pipe(htmlMin({
+            collapseWhitespace: true,
+        }))
+        .pipe(dest('dist'))
+}
+
+
+exports.styles = styles
+exports.htmlMinify = htmlMinify
